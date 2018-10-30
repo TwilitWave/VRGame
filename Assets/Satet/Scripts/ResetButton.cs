6eb60dc6a81e06642a61bd6afde3166929a6cf54
Tezika <tezikazhou@gmail.com>
2018-10-30 13:08:11 -0600

    add the reset panel

--- Assets/Satet/Scripts/ResetButton.cs
+++ Assets/Satet/Scripts/ResetButton.cs
@@ -1,32 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
+using SSR.Player;
 
-    public class ResetButton : MonoBehaviour
+public class ResetButton : MonoBehaviour
+{
+
+
+    public bool onceOnly = false;
+    public Transform targetCenter;
+
+    public Transform baseTransform;
+    public Transform fallenDownTransform;
+    public float fallTime = 0.5f;
+
+    const float targetRadius = 0.25f;
+
+    private bool targetEnabled = true;
+    // Use this for initialization
+    private void ApplyDamage()
     {
-       // public UnityEvent onTakeDamage;
-
-        public bool onceOnly = false;
-        public Transform targetCenter;
-
-        public Transform baseTransform;
-        public Transform fallenDownTransform;
-        public float fallTime = 0.5f;
-
-        const float targetRadius = 0.25f;
-
-        private bool targetEnabled = true;
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        PlayerEntity.Instance.ReloadGame();
     }
+
+}
 
